3475caefc92e8258e4311468f035030a312e1355
sfmDev <sfmdeveloper@icloud.com>
2016-02-14 22:10:39 +0800

    Initial Commit

---
+++ LoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoginDemo
+//
+//  Created by 石峰铭 on 16/2/14.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoginDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginDemo
+//
+//  Created by 石峰铭 on 16/2/14.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
